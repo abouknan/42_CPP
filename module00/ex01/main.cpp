f9887b510327a3b87ebdd7c259043d4780884b32
Ali Bouknana <abouknan@e2r11p3.1337.ma>
2025-09-06 00:11:53 +0100

    Sat Sep  6 12:11:53 AM +01 2025

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main (void)
+class list
 {
-    
+    public:
+        int a;
+        std::string str;
+        list(){a = 1, str = "Ali Bouknana";}
+        list(int s, std::string ptr){a = s, str = ptr;}
+};
+
+int main ()
+{
+    list obj;
+    list obj1(2, "Aya Achy");
+
+    std::cout << obj.a << "\t" << obj.str <<std::endl;
+    std::cout << obj1.a << "\t" << obj1.str <<std::endl;
 }
