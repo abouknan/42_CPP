f78d7f1178c3dd66bfd1acce03018ab578efa80b
Ali Bouknana <abouknan@e2r11p3.1337.ma>
2025-09-06 00:12:16 +0100

    Sat Sep  6 12:12:16 AM +01 2025

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -3,13 +3,11 @@
 
 int main (int ac, char **av)
 {
-    int a;
-
     if (ac < 2)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
     else
     {
-        for (a = 1; a < ac; a++)
+        for (int a = 1; a < ac; a++)
         {
             for(int i = 0; av[a][i]; i++)
                 std::cout << (char)toupper(av[a][i]);
