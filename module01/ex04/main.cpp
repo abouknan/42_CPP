459d3aa7555266da3464f4ac7adfe2f0d7e70554
Ali Bouknana <abouknan@e1r13p1.1337.ma>
2025-09-20 04:02:48 +0100

    Sat Sep 20 04:02:48 AM +01 2025

---
+++ module01/ex04/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+#define BLUE    "\033[34m"
+#define RED     "\033[31m"
+#define RESET   "\033[0m"
+
+int main (int ac, char **av)
+{
+    std::string tmp;
+    std::string line;
+    std::ifstream inputFile;
+
+    if (ac != 4)
+        return 1;
+    inputFile.open(av[1]);
+    if (!inputFile.is_open())
+        return  std::cerr << RED << "Error opening file!" << RESET << std::endl, 1;
+    if (!std::getline(inputFile, line))
+        return 1;
+    while (1)
+    {
+        line.append(tmp);
+        if (!std::getline(inputFile, tmp))
+            break;
+        line += "\n";
+    }
+    size_t foundPos = line.find(av[2]);
+    if (foundPos == std::string::npos)
+        return std::cout << RED << "The word '" << av[2] 
+        << "' was not found." << RESET << std::endl, 1;
+    std::cout << foundPos << std::endl;
+    // std::cout << line << std::endl;
+    inputFile.close();
+}
