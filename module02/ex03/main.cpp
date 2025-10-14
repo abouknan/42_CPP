d6c403f3c5ece9d3e2af579f7562fa19903de8ca
Ali Bouknana <abouknan@e2r11p3.1337.ma>
2025-10-14 05:02:30 +0100

    Tue Oct 14 05:02:30 AM +01 2025

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -9,10 +9,12 @@ int main(void)
     Point inside(2.5f, 2);
     Point outside(0, 5);
     Point onEdge(2.5f, 0);
+    Point vertex(0, 0);
     
     std::cout << "Point inside triangle: " << (bsp(a, b, c, inside) ? "YES" : "NO") << std::endl;
     std::cout << "Point outside triangle: " << (bsp(a, b, c, outside) ? "YES" : "NO") << std::endl;
     std::cout << "Point on edge: " << (bsp(a, b, c, onEdge) ? "YES" : "NO") << std::endl;
+    std::cout << "Point a vertex: " << (bsp(a, b, c, vertex) ? "YES" : "NO") << std::endl;
     
     return 0;
 }
