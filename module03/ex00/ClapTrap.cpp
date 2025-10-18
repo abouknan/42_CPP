d60c404efd474cf6ace185cad7111a02f7be8cce
Ali <macbookpro@MacBook-Pro-5.local>
2025-10-18 20:50:47 +0100

    Sat Oct 18 08:50:47 PM +0100 2025

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -31,8 +31,18 @@ ClapTrap& ClapTrap::operator=(ClapTrap const& other)
 
 void ClapTrap::attack(const std::string& target)
 {
-    if (this->HP <= 0 || this->EP <= 0)
-        return ;
+    if (HP <= 0)
+    {
+        std::cout << "ClapTrap " << name 
+            << " can’t attack because it has no hit points left!" << std::endl;
+        return;
+    }
+    if (EP <= 0)
+    {
+        std::cout << "ClapTrap " << name 
+            << " can’t attack because it has no energy points left!" << std::endl;
+        return;
+    }
     this->EP--;
     std::cout << "ClapTrap " << this->name << " attacks " << target 
         << ", causing " << this->AD << " points of damage!" << std::endl;
@@ -40,22 +50,31 @@ void ClapTrap::attack(const std::string& target)
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-    if (this->HP < amount || this->HP <= 0)
+    if (HP <= 0)
     {
-        this->HP = 0;
-        std::cout << "ClapTrap " << this->name << " takes " << amount 
-          << " points of damage! HP is now " << this->HP << "." << std::endl;
-        return ;
+        std::cout << "ClapTrap " << name << " can’t take more damage — it’s already destroyed!" << std::endl;
+        return;
     }
-    this->HP -= amount;
+    if (this->HP < amount)
+        this->HP = 0;
+    else
+        this->HP -= amount;
     std::cout << "ClapTrap " << this->name << " takes " << amount 
           << " points of damage! HP is now " << this->HP << "." << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-    if (this->HP <= 0 || this->EP <= 0)
-        return ;
+    if (HP <= 0)
+    {
+        std::cout << "ClapTrap " << name << " can’t be repaired — it’s destroyed!" << std::endl;
+        return;
+    }
+    if (EP <= 0)
+    {
+        std::cout << "ClapTrap " << name << " has no energy points left to repair itself!" << std::endl;
+        return;
+    }
     this->HP += amount;
     this->EP--;
     std::cout << "ClapTrap " << this->name << " is repaired by " << amount 
