e308eaebabec4359704250a8d22374385946f7b2
Ali Bouknana <abouknan@c4r9p5.1337.ma>
2026-07-24 19:29:04 +0100

    AA

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include "iter.hpp"
 
-void printInt(const int &x) { std::cout << x << std::endl; }
+void printInt(const int &x) { std::cout << x << "\t"; }
 void addOne(int &x) { x += 1; }
+void printString(std::string &s) { std::cout << "String: " << s << std::endl; }
 
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
     std::cout << "Original array:" << std::endl;
     iter(arr, 5, printInt);
+    std::cout << std::endl;
 
     iter(arr, 5, addOne);
     std::cout << "After addOne:" << std::endl;
     iter(arr, 5, printInt);
+    std::cout << std::endl;
 
     const std::string strs[] = {"hello", "world"};
-    iter(const_cast<std::string*>(strs), 2, [](std::string &s){ std::cout << s << std::endl; });
+    iter(const_cast<std::string*>(strs), 2, printString);
 
     return 0;
 }
